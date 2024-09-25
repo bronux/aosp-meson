f5679ec44b37c0dd3f92e531a74da9685e62af16
Theo Paris <theo@tinted.dev>
2024-09-24 17:09:12 -0700

    Squashed 'deps/art/' content from commit 3c4f9761bc
    git-subtree-dir: deps/art
    git-subtree-split: 3c4f9761bca89108b75b4d132bcb243c9a7e7f43

---
+++ runtime/arch/context.cc
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "context-inl.h"
+
+namespace art HIDDEN {
+
+Context* Context::Create() {
+  return new RuntimeContextType;
+}
+
+extern "C" void artContextCopyForLongJump(Context* context, uintptr_t* gprs, uintptr_t* fprs) {
+  context->CopyContextTo(gprs, fprs);
+  // Once the context has been copied, it is no longer needed.
+  // The context pointer is passed via hand-written assembly stubs, otherwise we'd take the
+  // context argument as a `std::unique_ptr<>` to indicate the ownership handover.
+  delete context;
+}
+
+}  // namespace art
